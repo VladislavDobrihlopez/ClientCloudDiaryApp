47010dddfb04620cb82855cad64b02e203ac0252
Vladislav Voitov <dobrihlopez@gmail.com>
2023-07-06 16:21:07 +0300

    add new animation when gallery section appearing

--- app/src/main/java/com/example/yourdiabetesdiary/presentation/screens/home/DiaryEntryHolder.kt
+++ app/src/main/java/com/example/yourdiabetesdiary/presentation/screens/home/DiaryEntryHolder.kt
@@ -1,6 +1,10 @@
 package com.example.yourdiabetesdiary.presentation.screens.home
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -98,13 +102,22 @@ fun DiaryEntryHolder(entry: DiaryEntry, onClick: (String) -> Unit) {
                     overflow = TextOverflow.Ellipsis
                 )
                 if (entry.images.isNotEmpty()) {
-                    GalleryDisplayManager(
+                    ShouldDisplayOnOffSwitch(
                         isDisplayed = galleryOpened,
                         onClick = {
                             galleryOpened.value = !galleryOpened.value
                         }
                     )
-                    AnimatedVisibility(visible = galleryOpened.value) {
+                    AnimatedVisibility(
+                        visible = galleryOpened.value,
+                        enter = fadeIn() +
+                                expandVertically(
+                                    animationSpec = spring(
+                                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                                        stiffness = Spring.StiffnessLow
+                                    )
+                                )
+                    ) {
                         Column(modifier = Modifier.padding(all = 14.dp)) {
                             Gallery(images = entry.images)
                         }
@@ -116,7 +129,7 @@ fun DiaryEntryHolder(entry: DiaryEntry, onClick: (String) -> Unit) {
 }
 
 @Composable
-private fun GalleryDisplayManager(isDisplayed: State<Boolean>, onClick: () -> Unit) {
+private fun ShouldDisplayOnOffSwitch(isDisplayed: State<Boolean>, onClick: () -> Unit) {
     TextButton(
         onClick = {
             onClick()
