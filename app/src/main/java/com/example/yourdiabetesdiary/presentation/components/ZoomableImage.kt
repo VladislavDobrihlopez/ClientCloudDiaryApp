ea0f5b6dc10c5a46605865d6e1aecc41a14da91e
Vladislav Voitov <dobrihlopez@gmail.com>
2023-07-14 23:28:32 +0300

    feat: add zooming gallery images

---
+++ app/src/main/java/com/example/yourdiabetesdiary/presentation/components/ZoomableImage.kt
@@ -0,0 +1,90 @@
+package com.example.yourdiabetesdiary.presentation.components
+
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun ZoomableImage(
+    url: Any,
+    onCloseClicked: () -> Unit,
+    onDeleteClicked: () -> Unit
+) {
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+    var scale by remember { mutableStateOf(1f) }
+    Box(
+        modifier = Modifier
+            .pointerInput(Unit) {
+                detectTransformGestures { _, pan, zoom, _ ->
+                    scale = maxOf(0.5f, minOf(scale * zoom, 7f))
+                    val maxX = (size.width * (scale - 1)) / 2
+                    val minX = -maxX
+                    offsetX = maxOf(minX, minOf(maxX, offsetX + pan.x))
+                    val maxY = (size.height * (scale - 1)) / 2
+                    val minY = -maxY
+                    offsetY = maxOf(minY, minOf(maxY, offsetY + pan.y))
+                }
+            }
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(
+                    transformOrigin = TransformOrigin(0.25f, 0.25f),
+                    scaleX = maxOf(.5f, minOf(3f, scale)),
+                    scaleY = maxOf(.5f, minOf(3f, scale)),
+                    translationX = offsetX,
+                    translationY = offsetY
+                ),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(url)
+                .crossfade(true)
+                .build(),
+            contentScale = ContentScale.Fit,
+            contentDescription = "Gallery Image"
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .padding(top = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = onCloseClicked) {
+                Icon(imageVector = Icons.Default.Close, contentDescription = "Close Icon")
+                Text(text = "Close")
+            }
+            Button(onClick = onDeleteClicked) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Icon")
+                Text(text = "Delete")
+            }
+        }
+    }
+}
